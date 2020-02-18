4a4663e6cb36aa5f3bb31488963872679f663085
Mattias Gullstrand <mattiasgullstrand@gmail.com>
2020-02-18 11:10:44 +0100

    Wip

---
+++ Assignment_2/RBF_Net_2dim.py
@@ -0,0 +1,139 @@
+import numpy as np
+import activation_functions_v2 as act_funs
+import matplotlib.pyplot as plt
+import random
+import math
+
+class RBF_Net_2dim():
+    """
+        - X is the input matrix where each column is a sample
+        - T is the target matrix where coulmn i is the target 
+            for the ith coulmn of X
+        - mus is a vector of the rbf centres
+        - sigmas is a vector of the rbf standard deviations
+        - weights is a vector of the weights for the weighted
+            sum to compute the output for a given x.
+        
+    """
+    def __init__(self, X, T, hidden_dim, weight_learn = 'ls', CL = True):
+        self.N = X.shape[1]
+        self.output_dim = T.shape[0]
+        self.hidden_dim = hidden_dim
+        self.D = X.shape[0]
+        self.X = X
+        self.T = T
+        self.eta = 0.01
+        self.errors = []
+        self.epochs = 1
+        self.mus = self.init_mus()
+        self.sigmas = self.init_sigmas()
+        self.weights = self.init_weights()
+        self.weight_learn = weight_learn
+        self.CL = CL
+        self.rbf_step_size = 0.2
+        self.rbf_updates = self.N
+        self.winners = 1
+    
+    def init_mus(self):
+        mu_grid = np.zeros([self.hidden_dim, 2], dtype = object)
+        for i in range(self.hidden_dim):
+            rand_idx = random.randrange(self.N)
+            rand_pattern = self.X[:,rand_idx]
+            mu_grid[i] = rand_pattern
+        return mu_grid
+    
+    def init_sigmas(self):
+        return np.ones(self.hidden_dim)
+    
+    def init_weights(self):
+        return np.random.rand(self.hidden_dim, 2)
+    
+    def calc_Phi(self, X):
+        # Specify which X
+        Phi = np.zeros([self.N, self.hidden_dim], dtype = object)
+        for i in range(self.N):
+            x = X[:,i].reshape(X.shape[0], 1)
+            for j in range(self.hidden_dim):
+                mu_j = self.mus[j]
+                sigma_j = self.sigmas[j]
+                Phi[i,j] = self.gaussian_rbf(x.flatten(), mu_j, sigma_j)
+        return Phi
+    
+    def gaussian_rbf(self, x, mu, sigma):
+        vec = -np.square(x - mu)/(2*np.square(sigma))
+        exp_vec = np.array([math.exp(i) for i in vec])
+        return exp_vec
+        #return np.exp(-np.square(x - mu)/(2*np.square(sigma)))
+    
+    def learn_weights_least_squares(self):
+        # Learn weights in batch mode, taking all samples
+        # into consideration per update. Using pseudo-inverse. 
+        Phi = self.calc_Phi(self.X)
+        f = self.T
+        Phi_sq_inv = np.linalg.pinv(np.matmul(Phi.T, Phi))
+        Phi_t_f = np.matmul(Phi.T, f)
+        self.weights = np.matmul(Phi_sq_inv, Phi_t_f)
+    
+    def learn_weights_delta_rule(self):
+        # Incrementally update the weights with the delta rule
+        Phi = self.calc_Phi(self.X)
+        f = self.T
+        for k in range(self.N):
+            Phi_xk = Phi[k,:].reshape(Phi.shape[1], 1)
+            f_xk = f[k,:].reshape(f.shape[1],1)
+            out = np.dot(Phi_xk.T, self.weights)
+            e = f_xk - out.T
+            weight_delta = self.eta * np.dot(e, Phi_xk.T).T
+            self.weights = np.add(self.weights, weight_delta)
+    
+    def batch_learn_weights(self):
+        if self.weight_learn == 'delta':
+            self.learn_weights_delta_rule()
+        else:
+            self.learn_weights_least_squares()
+        Phi = self.calc_Phi(self.X)
+        net_out = np.dot(Phi, self.weights)
+        f = self.T
+        abs_model_diff = np.abs(net_out - f)
+        abs_residual = np.mean(abs_model_diff)
+        self.errors.append(abs_residual)
+    
+    def CL_rbf_units(self):
+        for update in range(self.rbf_updates):
+            rand_idx = random.randrange(self.N)
+            x = self.X[:,rand_idx].reshape(self.X.shape[0], 1).flatten()
+            distances = np.zeros([self.hidden_dim, 2], dtype = object)
+            for unit_idx in range(self.hidden_dim):
+                mu = self.mus[unit_idx]
+                dist = self.sq_2_norm(x, mu)
+                distances[unit_idx,:] = np.array([unit_idx, dist])
+            sorted_dists = distances[distances[:,1].argsort()]
+            unit_inds = sorted_dists[:self.winners,:][:,0]
+            for update_idx in unit_inds:
+                self.update_rbf_centre(x, update_idx)
+    
+    def update_rbf_centre(self, x, w_idx):
+        w = self.mus[int(w_idx)]
+        self.mus[int(w_idx)] = w + self.rbf_step_size*(x - w)
+    
+    def sq_2_norm(self, x1, x2):
+        return np.dot((x1 - x2), (x1 - x2))
+        
+    def square_transform(self, x):
+        return np.sign(x)
+    
+    def train(self):
+        for e in range(self.epochs):
+            if self.CL:
+                self.CL_rbf_units()
+            self.batch_learn_weights()
+    
+    def predict(self, X):
+        Phi = self.calc_Phi(X)
+        net_out = np.matmul(Phi, self.weights)
+        return net_out
+    
+    def plot_errors(self):
+        plt.plot(self.errors)
+        print("Obtained error", self.errors[-1:])
+    
---
+++ Assignment_2/activation_functions_v2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def shifted_sigmoid(x):
+    return 2/(1 + np.exp(-x)) - 1
+
+def shifted_sigmoid_prime(x):
+    return 0.5*np.multiply((1 + shifted_sigmoid(x)), (1 - shifted_sigmoid(x)))
+
+def gaussian_rbf(x, mu, sigma):
+    return np.exp(-np.square(x - mu)/(2*np.square(sigma)))
